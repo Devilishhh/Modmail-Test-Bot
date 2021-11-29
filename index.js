e9af6ff598720fb538c2fc8a6de8a4647146525e
Leon <56304535+Devilishhh@users.noreply.github.com>
2021-11-29 17:28:32 +0000

    Adding files and everything to make sure I am learning

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+const Database = require("@replit/database")
 const Discord = require('discord.js');
+const Commando = require('discord.js-commando');
+
+const client = new Commando.Client({
+    owner: '1234567890805636964249763880'
+});
